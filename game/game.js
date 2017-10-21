84603f9031b148b8304726e643d04fb83802f89e
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-10-21 09:21:05 +0200

    ik ben ver met mn snake game

--- game/game.js
+++ game/game.js
@@ -1,37 +1,54 @@
-var slang;
-var scl;
+var s;
+var scl = 20;
 
+var food;
 
 function setup() {
-  createCanvas(800,600);
-  slang = new Snake();
-  
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
 
 }
 
-function keyPressed() {
-  if (keyCode == UP_ARROW) {
-    slang.dir(0,-1);
-  } else if (keyCode == DOWN_ARROW) {
-      slang.dir(0,1);
-    } else if (keyCode == RIGHT_ARROW) {
-        slang.dir(1,0);
-    } else if (keyCode == LEFT_ARROW) {
-        slang.dir(-1,0);
-    }
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
 }
-
 
 
 function draw() {
-    background(50);
-    slang.update();
-    slang.show();
-    slang.check();
+  background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+
+  s.death();
+  s.update();
+  s.show();
+  s.bilal();
+
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
 
 
 
 
 
-
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
 }
--- game/snake.js
+++ game/snake.js
@@ -1,37 +1,83 @@
-function Snake()
-{
 
-  this.x = 100;
+function Snake() {
+  this.x = 0;
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
-  this.update = function () {
-      this.x = this.x + this.xspeed;
-      this.y = this.y + this.yspeed;
-  }
-  this.show = function () {
-    fill(250);
-    rect(this.x, this.y, 10, 10)
-  }
+  this.totaal = 0;
+  this.lengte = [];
+  this.kansen = 0;
+  this.kansenp = document.getElementById('kansen');
+  this.score = 0;
+  this.scorep = document.getElementById('scoren');
+  this.scorenonthoud;
 
-    this.dir = function (x, y) {
-        this.xspeed = x;
-        this.yspeed = y;
-
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.totaal++;
+      this.score += 1;
+      this.scorep.innerHTML = "Score " + this.score;
+      return true;
+    } else {
+      return false;
     }
+  }
 
+  this.dir = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
 
-
-    this.check = function () {
-      if (this.x >= 800) {
-          this.x = 0;
-      } else if (this.y >= 600) {
-        this.y = 0;
-      } else if (this.y <= 0) {
-        this.y = 600
-      } else if (this.x <= 0) {
-        this.x = 800
+  this.death = function() {
+    for (var i = 0; i < this.lengte.length; i++) {
+      var pos = this.lengte[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        this.score = 0;
+        this.kansen += 1;
+        this.kansenp.innerHTML = "pogingen " + this.kansen;
+        if (this.kansen != 3) {
+          this.totaal = 0;
+          this.lengte = [];
+        } else if (this.kansen = 3) {
+          window.location.href = "gameover/index.html"
       }
     }
+  }
 
+this.bilal = function () {
+  if (this.kansen >= 2) {
+      console.log("Nu voor het echie")
+      this.scorenonthoud = localStorage.setItem("score", this.score);
+  }
+}
+
+  this.update = function() {
+    for (var i = 0; i < this.lengte.length - 1; i++) {
+      this.lengte[i] = this.lengte[i + 1];
+    }
+    if (this.totaal >= 1) {
+      this.lengte[this.totaal - 1] = createVector(this.x, this.y);
+    }
+
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+  }
+
+  this.show = function() {
+    fill(255);
+    for (var i = 0; i < this.lengte.length; i++) {
+      rect(this.lengte[i].x, this.lengte[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+
+  }
+
+
+
+}
 }
